a649fd9f22e188f411fd9250b47effc3ae7138b1
yusufdeepwork <yusufdeepwork@gmail.com>
2020-09-30 21:39:16 +0300

    Updated .gitignore

--- App.js
+++ App.js
@@ -40,14 +40,14 @@ const App = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}> </Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionTitle}>Değişimnpm </Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
