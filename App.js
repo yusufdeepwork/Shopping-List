8024f60395f0acee5b6e2a2db632d66e3607d7dc
yusufdeepwork <yusufdeepwork@gmail.com>
2020-10-01 11:59:13 +0300

    Added ListItem for Listing Items.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 import Header from './components/Header';
+import uuid from 'uuid-random';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item}/>}
+      />
     </View>
   );
 };
---
+++ components/ListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+const ListItem = ({item}) => {
+  return (
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemView}>
+        <Text style={styles.ListItemText}>{item.text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ListItem;
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  ListItemText: {
+    fontSize: 18,
+  },
+});
